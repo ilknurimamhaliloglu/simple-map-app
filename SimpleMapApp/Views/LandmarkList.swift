d4c4dbf9cbf0dbd8c201b9e23346fa2e41052652
İlknur Çapar <ilknurimamhaliloglu@gmail.com>
2024-05-03 12:35:38 +0300

    update landmark list

--- SimpleMapApp/Views/LandmarkList.swift
+++ SimpleMapApp/Views/LandmarkList.swift
@@ -6,7 +6,7 @@ struct LandmarkList: View {
         NavigationSplitView {
             List(landmarks) { landmark in
                 NavigationLink {
-                    LandmarkDetail()
+                    LandmarkDetail(landmark: landmark)
                 } label: {
                     LandmarkRow(landmark: landmark)
                 }
