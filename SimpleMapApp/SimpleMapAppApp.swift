86456cf8f8869ff337f4cb4e444165fbf3ff99b3
İlknur Çapar <ilknurimamhaliloglu@gmail.com>
2024-05-03 13:56:35 +0300

    add modelData logics

--- SimpleMapApp/SimpleMapAppApp.swift
+++ SimpleMapApp/SimpleMapAppApp.swift
@@ -3,9 +3,12 @@ import SwiftUI
 // The @main attribute identifies the app’s entry point.
 @main
 struct SimpleMapAppApp: App {
+    @State private var modelData = ModelData()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(modelData)
         }
     }
 }
